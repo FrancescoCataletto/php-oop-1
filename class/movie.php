b2343db28340688e11dcde9fc4b2ed514ba34b0f
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-17 17:02:06 +0200

    declared constructor

--- class/movie.php
+++ class/movie.php
@@ -5,7 +5,11 @@ class Movie {
     public $genre;
     public $rating;
 
-    
+    public function __construct($_name, $_genre, $_rating){
+        $this->name = $_name;
+        $this->genre = $_genre;
+        $this->rating = $_rating;
+    }
 }
 
 ?>
